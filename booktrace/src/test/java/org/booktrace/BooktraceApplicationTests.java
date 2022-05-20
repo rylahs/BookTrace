433607be95657ce78f43dc4bf1a392005e6a5b41
Chan ho Moon <minsonyo@naver.com>
2022-05-20 23:37:28 +0900

    Init Spring Boot Project

---
+++ booktrace/src/test/java/org/booktrace/BooktraceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.booktrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooktraceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
